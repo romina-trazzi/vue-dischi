a234a858befa5954511ce18bcd4658dd4ea1f769
Romina Trazzi <romina.trazzi@posta.it>
2021-01-13 08:30:26 +0100

    Commit
    Con l'uso di push (ancora non funzionante)

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,24 +9,24 @@ In base a cosa scegliamo nella select vedremo i corrispondenti cd. */
 
 let app = new Vue ({
     el: "#app",
-    data : {
-        dischiList: [] 
+    data: {
+        dischiList: []  
     },
     mounted () {  
         axios.get("https://flynn.boolean.careers/exercises/api/array/music").then(response => {  
             // console.log(response)
-            const theDisco = response.data.response;  // otteniamo i dati di risposta e li salviamo nell'array            
-            this.dischiList = theDisco;
-            console.log(this.dischiList);
+            const theDisco = response.data.response;  // otteniamo i dati di risposta            
+            this.dischiList.push(theDisco); // aggiungiamo i dati all'array in data
+            console.log(this.dischiList);  
         })
         .catch(error => {
-            console.log("Error " + error) // gestiamo l'errore
-
+            console.log("Error: " + error) // gestiamo l'errore
         })
         
     }
 })
 
 
-
+                
+            
 
